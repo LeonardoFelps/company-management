48b7673183f93fbf4792cec05920cdc05bc16e35
Leonardo_Felps <leofevieira98@gmail.com>
2026-06-17 14:05:48 -0300

    chore: iniciar projeto Vue

---
+++ src/mocks/db.js
@@ -0,0 +1,50 @@
+export const companies = [
+  {
+    id: 1,
+    name: 'Empresa Alpha',
+    cnpj: '00.000.000/0001-00',
+    status: 'ativa',
+    users: [
+      {
+        id: 1,
+        name: 'Joao Silva',
+        email: 'joao@alpha.com',
+        role: 'Administrador',
+      },
+      {
+        id: 2,
+        name: 'Maria Souza',
+        email: 'maria@alpha.com',
+        role: 'Analista',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Empresa Beta',
+    cnpj: '11.111.111/0001-11',
+    status: 'inativa',
+    users: [
+      {
+        id: 3,
+        name: 'Mauro Santos',
+        email: 'mauro@beta.com',
+        role: 'Administrador',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Empresa Gama',
+    cnpj: '22.222.222/0001-22',
+    status: 'ativa',
+    users: [
+      {
+        id: 4,
+        name: 'Tiago Mendes',
+        email: 'tiago@gama.com',
+        role: 'Gerente',
+      },
+    ],
+  },
+]
